4b55ddff1ff0d3ac7fe2eaabdb267068686ed0ba
Marc Cuva <mjcuva@gmail.com>
2019-10-21 19:24:52 -0700

    Use request-promise-native

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const request = require('request');
+const request = require('request-promise-native');
 
 async function run() {
   try {
