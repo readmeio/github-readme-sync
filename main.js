af327534c3b62b6c3c9421202454d95b20d22694
Marc Cuva <mjcuva@gmail.com>
2019-10-21 19:59:57 -0700

    add path

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const request = require('request-promise-native');
 const fs = require('fs');
+const path = require('path');
 
 async function run() {
   try {
