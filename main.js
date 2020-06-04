bd9a19a4c6c236a3ca426ac4ac58c25f1ec7de4e
gkoberger <gkoberger@gmail.com>
2020-06-04 15:26:46 -0700

    bring back the consoles

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const OAS = require('oas-normalize');
 async function run() {
   let oasKey;
 
+  console.log(1);
+
   try {
     oasKey = core.getInput('readme-oas-key', { required: true });
   } catch(e) {
@@ -16,9 +18,11 @@ async function run() {
       'You need to set your key in secrets! In the repo, go to Settings > Secrets and add README_OAS_KEY. You can get the value from your ReadMe account.'
     );
   }
+  console.log(2);
 
   const readmeKey = oasKey.split(':')[0];
   const apiSettingId = oasKey.split(':')[1];
+  console.log(3);
 
   /*
   const apiFilePath = core.getInput('api-file-path', { required: true });
@@ -26,6 +30,7 @@ async function run() {
   const apiVersion = core.getInput('readme-api-version', { required: true });
   */
 
+  console.log(4);
   var base = 'swagger.yaml'; // TODO! fix this;
   swaggerInline('**/*', {
     format: '.json',
@@ -33,6 +38,7 @@ async function run() {
     base,
   }).then(generatedSwaggerString => {
     const oas = new OAS(generatedSwaggerString);
+  console.log(5);
 
     oas.bundle(function (err, schema) {
       if (!schema['x-si-base']) {
@@ -44,6 +50,7 @@ async function run() {
         */
       }
 
+  console.log(6);
       const options = {
         formData: {
           spec: {
@@ -65,6 +72,7 @@ async function run() {
 
       // TODO: Validate it here?
 
+  console.log(7);
       return request
         .put(
           `https://dash.readme.io/api/v1/api-specification/${apiSettingId}`,
@@ -75,11 +83,14 @@ async function run() {
             return 'Success!';
           },
           err => {
+  console.log(8);
             if (err.statusCode === 503) {
+  console.log(9);
               core.setFailed(
                 'Uh oh! There was an unexpected error uploading your file. Contact support@readme.io with a copy of your file for help!',
               );
             } else {
+  console.log(10);
               console.log(err.message);
               core.setFailed(err.message);
             }
