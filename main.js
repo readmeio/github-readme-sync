8ad0f45f06a9ef3999bdc18f3b839713193992f1
Kanad Gupta <kanad@readme.io>
2020-10-19 13:45:36 -0500

    feat: OAS validation, better error handling, action debugging (#37)
    * ci: add prettier to package.json
    
    This allows us to better utilize the prettier config locally!
    
    * fix: error out if no file is found
    
    * ci: add back warnings for console.logs()
    
    We should use the core.info() statements instead!
    
    Info: https://github.com/actions/toolkit/tree/master/packages/core#logging
    
    * feat: validate OAS before bundling
    
    * docs: update some log messages
    
    Convert a console.log() to core.info() and update some language
    
    * refactor: remove an unnecessary if block
    
    Now that we're validating the OAS beforehand, this is unnecessary.
    
    Also removes a now-completed TODO :tada:
    
    * fix: properly parse our error messages
    
    Refactors the parsing logic to handle the new error message structure, and also adds a few additional helpful error messages for certain errors.
    
    Also slightly refactored the version logic so we can use version in one of the error messages.
    
    * feat: add debugging
    
    GitHub Actions support debugging which may be useful for customers that have issues, so I added a bunch of debug statements. And no, they won't get logged by default!
    
    Info: https://github.com/actions/toolkit/blob/master/docs/action-debugging.md
    
    Also I added logic to the demo executor to better log these debug lines, and refactored some of the output logic.
    
    * deps: try commiting node_modules
    
    Because of an error I'm seeing here: https://github.com/kanadgupta/metrotransit-nextrip-oas/runs/1261522969
    
    * fix: move split into try catch
    
    * docs: improve some error messages, add a link to our docs
    
    * fix: sanitize all API keys, etc. before writing to stdout
    
    * chore: remove ReadMe API key entirely from error message
    
    sanitizing it won't really help in this case! having a placeholder should make it clearer
    
    * chore: oops
    
    we can't use the function before it's defined!

--- main.js
+++ main.js
@@ -8,29 +8,66 @@ const promisify = require('util').promisify;
 
 const globPromise = promisify(glob);
 
+function sanitize(input) {
+  return input.substr(0, 3) + input.substr(3, input.length - 6).replace(/\w/g, '·') + input.substr(input.length - 3);
+}
+
 async function run() {
   let oasKey;
+  let readmeKey;
+  let apiSettingId;
 
   try {
     oasKey = core.getInput('readme-oas-key', { required: true });
+    readmeKey = oasKey.split(':')[0];
+    apiSettingId = oasKey.split(':')[1];
   } catch (e) {
     core.setFailed(
       'You need to set your key in secrets!\n\nIn the repo, go to Settings > Secrets and add README_OAS_KEY. You can get the value from your ReadMe account.'
     );
   }
 
-  const readmeKey = oasKey.split(':')[0];
-  const apiSettingId = oasKey.split(':')[1];
+  function sanitizeKeys(input) {
+    // Sanitize ReadMe API Key
+    const keySanitized = sanitize(readmeKey);
+    let sanitizedInput = input.replace(new RegExp(readmeKey, 'g'), keySanitized);
+
+    // Sanitize Spec ID
+    const specIdSanitized = sanitize(apiSettingId);
+    sanitizedInput = sanitizedInput.replace(new RegExp(apiSettingId, 'g'), specIdSanitized);
+
+    return sanitizedInput;
+  }
+
+  function debug(input) {
+    const sanitizedInput = sanitizeKeys(input);
+    return core.debug(sanitizedInput);
+  }
+
+  function setFailed(input) {
+    const sanitizedInput = sanitizeKeys(input);
+    return core.setFailed(sanitizedInput);
+  }
+
+  debug(`readmeKey (split from \`readme-oas-key\`): ${readmeKey}`);
+  debug(`apiSettingId (split from \`readme-oas-key\`): ${apiSettingId}`);
 
   const apiVersion = core.getInput('api-version');
+  debug(`apiVersion (from \`api-version\` input): ${apiVersion}`);
   const apiFilePath = core.getInput('oas-file-path');
+  debug(`apiFilePath (from \`oas-file-path\` input): ${apiFilePath}`);
 
   let baseFile = apiFilePath;
 
   if (!baseFile) {
     const files = await globPromise('**/{swagger,oas,openapi}.{json,yaml,yml}', { dot: true });
+    debug(`${files.length} file match(es) found: ${files.toString()}`);
+    if (!files.length)
+      throw setFailed(
+        'Unable to locate a OpenAPI/Swagger file. Try specifying the path via the `oas-file-path` option in your workflow file!'
+      );
     baseFile = files[0];
-    console.log(`Found spec file: ${baseFile}`);
+    core.info(`OpenAPI/Swagger file found: ${baseFile}`);
   }
 
   swaggerInline('**/*', {
@@ -39,21 +76,27 @@ async function run() {
     base: baseFile,
     ignoreErrors: true,
   })
-    .then(generatedSwaggerString => {
+    .then(async generatedSwaggerString => {
       const oas = new OAS(generatedSwaggerString);
+      const validate = promisify(oas.validate);
+      try {
+        await validate.call(oas);
+      } catch (e) {
+        debug(`Error validating spec: ${e}`);
+        const innerMessage = e.errors && e.errors[0] && e.errors[0].message;
+        const outerMessage = e.name ? `${e.name}: ${e.message}` : e.message;
+        throw setFailed(`There was an error validating your OAS file.\n\n${innerMessage || outerMessage || e}`);
+      }
+      debug('OpenAPI/Swagger file validated!');
 
       oas.bundle(function (err, schema) {
-        if (!schema['x-si-base']) {
-          // TODO: Put this back
-          /*
-        console.log("We couldn't find a Swagger file.".red);
-        console.log(`Don't worry, it's easy to get started! Run ${'oas init'.yellow} to get started.`);
-        process.exit(1);
-        */
-        }
-
         schema['x-github-repo'] = process.env.GITHUB_REPOSITORY;
+        debug(`\`x-github-repo\`: ${schema['x-github-repo']}`);
         schema['x-github-sha'] = process.env.GITHUB_SHA;
+        debug(`\`x-github-sha\`: ${schema['x-github-sha']}`);
+
+        const version = apiVersion || schema.info.version;
+        debug(`Version passed into \`x-readme-version\` header: ${version}`);
 
         const options = {
           formData: {
@@ -66,14 +109,12 @@ async function run() {
             },
           },
           headers: {
-            'x-readme-version': apiVersion || schema.info.version, // apiVersion,
+            'x-readme-version': version,
             'x-readme-source': 'github',
           },
           auth: { user: readmeKey },
           resolveWithFullResponse: true,
         };
-
-        // TODO: Validate it here?
 
         return request.put(`https://dash.readme.io/api/v1/api-specification/${apiSettingId}`, options).then(
           () => {
@@ -81,31 +122,52 @@ async function run() {
           },
           err => {
             if (err.statusCode === 503) {
-              core.setFailed(
-                'Uh oh! There was an unexpected error uploading your file. Contact support@readme.io with a copy of your file for help!'
+              setFailed(
+                'Uh oh! There was an unexpected error uploading your file. Contact support@readme.io with a copy of your file and debug logs for help!\n\nInfo: https://docs.readme.com/docs/automatically-sync-api-specification-with-github#troubleshooting'
               );
             } else {
+              debug(`Error received from ReadMe API: ${err}`);
               let errorOut = err.message;
+              let errorObj;
               try {
-                errorOut = JSON.parse(err.error).description;
+                errorObj = JSON.parse(err.error);
+                if (errorObj.message) {
+                  errorOut = errorObj.message;
+                  if (errorObj.suggestion) errorOut = `${errorOut}\n\n${errorObj.suggestion}`;
+                }
               } catch (e) {
-                // Should we do something here?
+                debug(`Error parsing error object: ${e}`);
+                throw setFailed(
+                  'Uh oh! There was an unexpected error uploading your file. Contact support@readme.io with a copy of your file and debug logs for help!\n\nInfo: https://docs.readme.com/docs/automatically-sync-api-specification-with-github#troubleshooting'
+                );
               }
 
-              if (errorOut.match(/no version/i)) {
-                // TODO: This is brittle; I'll fix it in the API tomorrrow then come back here
-                errorOut +=
-                  "\n\nBy default, we use the version in your OAS file, however this version isn't on ReadMe.\n\nTo override it, add `api-version: 'v1.0.0'` to your GitHub Action, or add this version in ReadMe!";
+              if (errorObj.error && errorObj.error === 'SPEC_VERSION_NOTFOUND') {
+                errorOut += `\n\nYou can specify this override by adding \`api-version: 'v1.0.0'\` to your GitHub Action (or by creating version '${version}' in ReadMe!)`;
+                errorOut += `\n\nDocs: https://docs.readme.com/docs/versions`;
               }
 
-              core.setFailed(errorOut);
+              if (errorObj.error && errorObj.error === 'SPEC_ID_ALREADY_EXISTS') {
+                const match = errorOut.match(/(?<=(something like )).{24}/);
+                if (match) {
+                  const newId = match[0];
+                  errorOut += `\n\nJust set \`readme-oas-key\` to \`YOUR_README_API_KEY:${newId}\`!`;
+                } else
+                  errorOut += `\n\nThe spec ID is located in the second half of the \`readme-oas-key\` after the colon separator.`;
+              }
+
+              if (errorObj.error && errorObj.error === 'INTERNAL_ERROR') {
+                errorOut += `\n\nWe'll be able to solve the problem faster if you include a copy your debug logs! Info: https://docs.readme.com/docs/automatically-sync-api-specification-with-github#troubleshooting`;
+              }
+
+              setFailed(errorOut);
             }
           }
         );
       });
     })
     .catch(err => {
-      core.setFailed(`There was an error finding or loading your OAS file.\n\n${err.message || err}`);
+      setFailed(`There was an error finding or loading your OpenAPI/Swagger file.\n\n${err.message || err}`);
     });
 }
 
